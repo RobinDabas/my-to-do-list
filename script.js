02d8b0cf7e82be7973dd10a2c8919fbdfc00fe6e
ROBERTO D <robindabas@hotmail.com>
2025-06-24 13:35:01 -0400

    tested my knowledge on creating a simple to-do list and I passed woohoo

---
+++ script.js
@@ -0,0 +1,15 @@
+function addTask(){
+  const taskInput = document.getElementById('taskInput');
+  const taskText = taskInput.value.trim();
+  if(taskText !== '') {
+    const li = document.createElement('li');
+    li.textContent = taskText;
+    document.getElementById('taskList').appendChild(li);
+    textInput.value = '';
+  }
+}
+
+function deleteTask() {
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = '';
+}
