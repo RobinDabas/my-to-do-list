2745f50ed582c66a48827f091e0fe894ee189fc5
ROBERTO D <robindabas@hotmail.com>
2025-06-26 11:12:19 -0400

    practicing recursive functions

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ function deleteAllTasks() {
   const taskList = document.getElementById('taskList');
   taskList.innerHTML = '';
 }
+
+let contrarrestar = 3;
+function ejemplo() {
+  console.log(contrarrestar);
+  contrarrestar = contrarrestar - 1;
+  if(contrarrestar === 0) return;
+  ejemplo();
+}
+ejemplo();
+//console.log('linea uno');
+//console.log('linea dos');
+//console.log('linea tres');
+
+
+let horario = 3;
+function miDia() {
+  console.log(horario);
+  horario = horario - 1;
+  if(horario === 0) return;
+  miDia();
+}
+miDia();
+//console.log('Manana');
+//console.log('Tarde');
+//console.log('Noche')
+
+
+let salario = 5;
+while (salario > 0) {
+  console.log(salario);
+  salario--;
+}
