12271f5bbd22401c215770cb42bcb87e736ee5c1
ROBERTO D <robindabas@hotmail.com>
2025-06-29 16:27:47 -0400

    learned and practiced oop and fp concepts

--- script.js
+++ script.js
@@ -46,3 +46,72 @@ while (salario > 0) {
   console.log(salario);
   salario--;
 }
+
+function factorial(n) {
+  if(n === 0) return 1;
+  return n * factorial(n - 1);
+}
+console.log(factorial(6))
+
+
+
+function spanishSpeakingCaribbeanNations(esp) {
+  console.log('The Spanish-speaking Caribbean nations with the highest amount of European ancestry are ' + esp);
+}
+spanishSpeakingCaribbeanNations('Cuba and Puerto Rico.')
+
+
+
+function monthlyEarnings(eBooks, dropshipping) {
+  return eBooks + dropshipping;
+}
+
+var eBooks = 2000;
+var dropshipping = 5000;
+
+var yearlyIncome = monthlyEarnings(eBooks, dropshipping) * 12;
+
+console.log();
+yearlyIncome
+
+
+/*OOP:Create an object (e.g., car) with properties (speed) and methods (accelerate).
+Use this to update properties.
+Example: car.accelerate() increases speed.
+
+/*
+function car() {
+  this.speed = 0;
+  this.accelerate = function() {
+    this.speed += 10;
+  };
+}
+
+let myCar = new car()
+console.log(myCar.speed);
+myCar.accelerate();
+console.log(myCar.speed); 
+*/
+
+var farmer = {
+  horse: 10,
+  feed: function() {
+    this.horse -= 2;
+  }
+} 
+
+console.log(farmer.horse);
+farmer.feed();
+console.log(farmer.horse);
+
+
+//FP
+
+function calculateTotal(income, expenses) {
+  return income - expenses
+}
+var income = 8500;
+var expenses = 2000;
+
+var monthlyNetEarnings = calculateTotal(income, expenses);
+console.log(monthlyNetEarnings);
